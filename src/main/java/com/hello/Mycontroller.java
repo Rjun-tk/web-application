c7c4b644b0d00b05f11e53e16761ac90064fc4eb
Mallikarjuna <mallikarjuna@tekenlight.com>
2025-03-25 16:20:50 +0530

    first commit

---
+++ src/main/java/com/hello/Mycontroller.java
@@ -0,0 +1,27 @@
+package com.hello;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+public class MyController {
+	
+	
+	@GetMapping("/hello")
+	public ResponseEntity<?> getHello(){
+		return ResponseEntity.ok("Hello ! endpoint called ");
+	}
+	
+	
+	@GetMapping("/")
+	public ResponseEntity<?> getIndexPage(){
+		return ResponseEntity.ok("Hello ! Index Page Called");
+	}
+	
+	@GetMapping("/{name}")
+	public ResponseEntity<?> getName(@PathVariable(name = "its Working ",required = false) String name){
+		return ResponseEntity.ok("Hello !"+name +" :)");
+	}
+
+}
+
